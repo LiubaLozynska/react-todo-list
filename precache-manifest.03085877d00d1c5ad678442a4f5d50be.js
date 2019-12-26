b4e6fc16e9452aa293c9d87b4aa6f65eb99a7f78
unknown <liuba.lozynska@gmail.com>
2019-12-26 22:40:31 +0200

    Updates

---
+++ precache-manifest.03085877d00d1c5ad678442a4f5d50be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7700e7fa4deda9b778072a4b84ff8c4",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "bd9e80ebca569ad93fa5",
+    "url": "/react-todo-list/static/css/main.3dc5dd61.chunk.css"
+  },
+  {
+    "revision": "b2d25bc4d658ccb68299",
+    "url": "/react-todo-list/static/js/2.0a74d130.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-list/static/js/2.0a74d130.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd9e80ebca569ad93fa5",
+    "url": "/react-todo-list/static/js/main.d1dd2495.chunk.js"
+  },
+  {
+    "revision": "d969d0a3f167dfdf3523",
+    "url": "/react-todo-list/static/js/runtime-main.10c495f4.js"
+  }
+]);
